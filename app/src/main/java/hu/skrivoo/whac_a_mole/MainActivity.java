3dc9187620a34054715bd40e975eaba25426b408
Skrivoo <pedrusz96@gmail.com>
2022-03-30 19:24:22 +0200

    Initial commit

---
+++ app/src/main/java/hu/skrivoo/whac_a_mole/MainActivity.java
@@ -0,0 +1,14 @@
+package hu.skrivoo.whac_a_mole;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
