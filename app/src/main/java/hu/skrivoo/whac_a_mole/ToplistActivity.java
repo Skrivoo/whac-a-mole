f5288265f0d49f15a0d13c0ec42d9d4dc97260e7
Skrivoo <pedrusz96@gmail.com>
2022-04-11 14:38:23 +0200

    gameactivity bg átállítva fix képről

--- app/src/main/java/hu/skrivoo/whac_a_mole/ToplistActivity.java
+++ app/src/main/java/hu/skrivoo/whac_a_mole/ToplistActivity.java
@@ -4,11 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
 public class ToplistActivity extends AppCompatActivity {
+
+    private FirebaseFirestore firestore;
+    private CollectionReference players;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toplist);
+        firestore = FirebaseFirestore.getInstance();
+        players = firestore.collection("score");
     }
+
+
+
 }
