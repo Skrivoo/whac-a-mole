2d221b15ba7b160e5babe29944993b552818e5dc
Skrivoo <pedrusz96@gmail.com>
2022-04-24 20:04:01 +0200

    bugfix

--- app/src/main/java/hu/skrivoo/whac_a_mole/Mole.java
+++ app/src/main/java/hu/skrivoo/whac_a_mole/Mole.java
@@ -3,18 +3,25 @@ package hu.skrivoo.whac_a_mole;
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 public class Mole {
 
+    private static final String LOG_TAG = Mole.class.getName();
     private ImageView moleView;
     private boolean active;
     @SuppressLint("ClickableViewAccessibility")
     private final View.OnTouchListener changeColorListener = (v, event) -> {
-        Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
-        int color = bmp.getPixel((int) event.getX(), (int) event.getY());
-        return color == Color.TRANSPARENT;
+        try {
+            Bitmap bmp = Bitmap.createBitmap(v.getDrawingCache());
+            int color = bmp.getPixel((int) event.getX(), (int) event.getY());
+            return color == Color.TRANSPARENT;
+        } catch (IllegalArgumentException e) {
+            Log.w(LOG_TAG, "Nagy a baj, :" + e.getMessage());
+        }
+        return false;
     };
 
     @SuppressLint("ClickableViewAccessibility")
